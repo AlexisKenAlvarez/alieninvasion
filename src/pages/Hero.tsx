f53e4fdb5b884fa942077eb97d64c69fe89133c1
Alexis Ken Alvarez <alexisken1432@gmail.com>
2023-06-11 15:53:10 +0800

    Finished but no nav function yet

--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -7,6 +7,7 @@ import HeroAbout from "../views/Hero/HeroAbout";
 import HeroInterview from "../views/Hero/HeroInterview";
 import HeroDeeper from "../views/Hero/HeroDeeper";
 import Tokenomics from "../views/Hero/Tokenomics";
+import Footer from "../components/Footer";
 
 const Hero = () => {
 
@@ -26,6 +27,7 @@ const Hero = () => {
                         <HeroDeeper />
                         <HeroInterview />
                         <Tokenomics />
+                        <Footer />
 
                     </>
                     : <VideoPlayer key="VIDEOENDED" handleEnded={handleEnded} />}
